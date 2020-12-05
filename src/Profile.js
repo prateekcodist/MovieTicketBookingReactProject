123448e64a8f2edbb01553002444780ace8fd220
AnubhavHawk <48518205+AnubhavHawk@users.noreply.github.com>
2020-12-05 18:38:54 +0530

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -14,7 +14,7 @@ class Profile extends React.Component {
   componentDidMount() {
     var self = this;
     axios
-      .get(`http://localhost:8082/customer/viewbyId/6`)
+      .get(`http://localhost:8082/customer/viewbyId/${localStorage.userId}`)
       .then(function (response) {
         console.log(response);
         self.setState({
@@ -31,41 +31,28 @@ class Profile extends React.Component {
   render() {
     return (
       <div>
-        <div className="bg-light p-3 row" style={{ height: "100vh" }}>
-          <div className="shadow-sm pt-3 mt-3 bg-white rounded col-sm-4 offset-4 text-center">
-            <img
-              src="https://icon-library.com/images/facebook-user-icon/facebook-user-icon-4.jpg"
-              className="rounded-circle"
-              height="100px"
-              width="100px"
-              alt=".."
-            />
-            <hr />
-            <div className="mt-3">
-              <b>Name: </b>
-              {this.state.username}
-              <br />
-              <b>Contact: </b>
-              {this.state.contact}
-            </div>
-            <div className="mt-4">
-              <h6 className="text-left text-secondary">Booked Tickets</h6>
-              {this.state.ticketlist.length !== 0 ? (
-                <ul>
-                  {this.state.ticketlist.map((ticket) => (
-                    <li>{ticket.ticketId}</li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="text-danger">
-                  Looks like you haven't booked any ticket yet
-                </p>
-              )}
-            </div>
+          <div className="bg-light p-3 row" style={{height:'100vh'}}>
+                <div className="shadow-sm pt-3 mt-3 bg-white rounded col-sm-4 offset-4 text-center">
+                    <img src="https://icon-library.com/images/facebook-user-icon/facebook-user-icon-4.jpg" className="rounded-circle" height="100px" width="100px" alt=".."/>
+                    <hr/>
+                    <div className="mt-3">
+                        <b>Name: </b>{this.state.username}
+                        <br/>
+                        <b>Contact: </b>{this.state.contact}
+                    </div>
+                    <div className="mt-4">
+                        <h6 className="text-left text-secondary">Booked Tickets</h6>
+                        {
+                            this.state.ticketlist.length !== 0 ? <ul>
+                                {this.state.ticketlist.map(ticket => <li>{ticket.ticketId}</li>) }</ul>
+                            : <p className="text-danger">Looks like you haven't booked any ticket yet</p>
+                        }
+                    </div>
+                </div>
+
           </div>
-        </div>
       </div>
     );
   }
 }
-export default Profile;
+export default Profile
