2a782ecaf55ca19f03da0f72a0f54d593b223d53
AnubhavHawk <48518205+AnubhavHawk@users.noreply.github.com>
2020-12-05 18:33:32 +0530

    Login validations and comments added

--- src/Login.js
+++ src/Login.js
@@ -11,46 +11,64 @@ class Login extends React.Component{
                 password: '',
                 role: ''
             },
-            errorMessage: ''
+            errorMessage: '',
+            disp:'password',
+            text:'show'
         }
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.toggle = this.toggle.bind(this);
+    }
+    // A function to achieve the show-hide password function
+    toggle(){
+        if(this.state.disp === 'password'){
+            this.setState({disp:'text', text: 'hide'})
+        }
+        else
+            this.setState({disp:'password', text: 'show'})
     }
     handleSubmit = async (e) => {
         e.preventDefault();
+
+        // RegExp for only numeric user Id
+        let idPattern = new RegExp("^[0-9]{1,}$");
+        if(!idPattern.test(this.state.param.userId)){
+            this.setState({errorMessage:"Please enter a valid numeric userId"})
+            return false;
+        }
+        if(this.state.param.password === ''){ // Check if the password is empty
+            this.setState({errorMessage:"Password can't be empty"})
+            return false;
+        }
         console.log(this.state.param)
         let resp = null
         try{
-            resp = await axios.post('http://localhost:8082/user/signin', this.state.param)
+            resp = await axios.post('http://localhost:8082/user/signin', this.state.param) // authenticate user on the server
         }
         catch(err){
             console.log("Error: ",  err.response.data);
             this.setState({errorMessage: 'Invalid User Id or Password'})
-            // alert('Invalid User Id or password');
         }
         console.log(resp);
         if(resp != null){
-            localStorage.setItem('role', resp.data.role);
-            localStorage.setItem('userId', this.state.param.userId);
-            window.location = '/home';
+            localStorage.setItem('role', resp.data.role); // set role (admin/customer) into localStorage
+            localStorage.setItem('userId', this.state.param.userId); // set UserId into localStorage
+            window.location = '/home'; // send the user to the home page.
         }
-            // this.props.createAuth();
-            // return <Redirect to="home" />
-        // if(resp !== null)
-        //     window.location = '/home';
     }
     render(){
         return (
             <div className="bg-light text-center row p-5">
                 <div className="mt-5 bg-white  p-4 border border-primary rounded col-md-4 offset-md-4 shadow">
-                    <p className="text-danger">{this.state.errorMessage}</p>
+                    <p className="text-danger" onClick={e => this.setState({errorMessage: ''})}>{this.state.errorMessage}</p>
                     <form className="text-left" onSubmit={this.handleSubmit}>
                         <div className="form-group">
                             <label>User Id <small>(As provided during register)</small></label>
-                            <input type="text" className="form-control" onChange={(e)=> {this.state.param.userId=e.target.value; this.setState({errorMessage: ''})}} required/>
+                            <input type="text" className="form-control" onChange={(e)=> {this.state.param.userId=e.target.value;}} required/>
                         </div>
                         <div className="form-group">
                             <label>Password</label>
-                            <input type="text" className="form-control" onChange={(e) => this.state.param.password=e.target.value} required/>
+                            <input type={this.state.disp} className="form-control" onChange={(e) => this.state.param.password=e.target.value} required/>
+                            <small className="text-primary" style={{cursor:'pointer'}} onClick={this.toggle}>{this.state.text} password</small>
                         </div>
                         <div className="form-group text-center">
                             <input type="submit" value="login" className="btn pl-5 pr-5 rounded-pill btn-primary" required/>
